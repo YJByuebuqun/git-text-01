b923827fd4566cff03cf8ad0f65323c0f0a0ccaa
yanjiabo <yanjiabo@yjb.com>
2022-09-05 23:19:34 +0800

    master-05 commit

--- src/main/java/com/yanjiabo/git/GitText.java
+++ src/main/java/com/yanjiabo/git/GitText.java
@@ -9,5 +9,6 @@ public class GitText {
         System.out.println("hello git 04 !!");
         System.out.println("master conflicts test !!");
         System.out.println("hot-fix conflicts test !!");
+        System.out.println("push test !!");
     }
 }
