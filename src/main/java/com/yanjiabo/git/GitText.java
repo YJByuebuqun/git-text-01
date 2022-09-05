d6d62921af0a2b024f89d465eb7865b554712e0e
yanjiabo <yanjiabo@yjb.com>
2022-09-05 22:23:03 +0800

    thrid commit

--- src/main/java/com/yanjiabo/git/GitText.java
+++ src/main/java/com/yanjiabo/git/GitText.java
@@ -5,5 +5,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git !!");
         System.out.println("hello git 02 !!");
+        System.out.println("hello git 03 !!");
     }
 }
